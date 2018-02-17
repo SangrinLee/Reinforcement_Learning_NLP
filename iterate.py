4426564c2e0f98e777941844ea403185b04f399e
SangrinLee <sangrinlee2018@u.northwestern.edu>
2018-02-17 13:59:38 -0600

    added data loading

--- iterate.py
+++ iterate.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-type = "active"
+import matplotlib.pyplot as plt
+
+# type = "active"
 # type = "active_no_weight"
 # type = "baseline"
+type = "data_active"
 
 if type == "active":
 	import word_train_RL as train
@@ -11,6 +14,22 @@ elif type == "active_no_weight":
 elif type == "baseline":
 	import word_train_RL_baseline as train
 
+elif type == "data_active":
+	import rare_proportion as rare_prop
+	path = 'dataset/active learning'
+elif type == "data_active_no_weight":
+	import rare_proportion as rare_prop
+	path = 'dataset/active learning without weight'
+elif type == "data_active_baseline":
+	import rare_proportion as rare_prop
+	path = 'dataset/baseline'
+
+prop_list = []
 for i in range(11):
 	print ("===== Cut off " + str(i * 0.1) + " =====")
-	train.iterate(i * 0.1)
+	# train.iterate(i * 0.1)
+	prop_list.append(path, rare_prop.iterate(i * 0.1))
+
+print (type)
+plt.plot(prop_list)
+plt.savefig("rare_proportion_" + type + '.png')
