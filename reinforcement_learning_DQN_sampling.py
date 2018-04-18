ca7c1ffe319922878f885172917479f2925cb6e8
SangrinLee <sangrinlee2018@u.northwestern.edu>
2018-04-17 21:59:51 -0500

    updated create feature

--- reinforcement_learning_DQN_sampling.py
+++ reinforcement_learning_DQN_sampling.py
@@ -13,56 +13,45 @@ from extract_sentences import train, val, ptb_dict, extract_sentence_list
 sentence_list = extract_sentence_list(train)
 sentence_list = sentence_list[len(sentence_list)//2:]   # First half used for LSTM training
 
-# Construct the batchified list
-def select_batch(sentence_list):
-    # shuffle the sentence list for removing the ordering of the document
-    # this is to make independent sentences as the task is more like the sentence focused, not the document
-    np.random.shuffle(sentence_list)
-    sentence_list = np.concatenate(sentence_list)
-
-    # create the batch with the size of 60
-    nbatch = len(sentence_list) // 60
-    sentence_list = sentence_list[:nbatch*60]
-    batchified_list = np.split(sentence_list, nbatch)
-
-    return batchified_list
-
 # Create the feature by converting the sentence -> data (# of words, proportion of uni, bi, tri unseen before)
-def create_feature(data, uni_seen_list, bi_seen_list, tri_seen_list):
-    # unigram process
-    num_uni = len(data)
-    num_uni_unseen = 0
-    for uni in data:
-        if not uni in uni_seen_list:
-            num_uni_unseen += 1
-            uni_seen_list.append(uni)
-    prop_uni_unseen = num_uni_unseen / num_uni # proportion of unseen unigram words
+def create_feature(data, uni_seen_list, bi_seen_list, tri_seen_list, doUpdate):
+	# unigram process
+	num_uni = len(data)
+	num_uni_unseen = 0
+	for uni in data:
+		if not uni in uni_seen_list:
+			num_uni_unseen += 1
+			if doUpdate:
+				uni_seen_list.append(uni)
+	prop_uni_unseen = num_uni_unseen / num_uni # proportion of unseen unigram words
 
-    # bigram process
-    num_bi = len(data) - 1
-    num_bi_unseen = 0
-    for i in range(num_bi):
-        bi = list(data[i:i+2])
-        if not bi in bi_seen_list:
-            num_bi_unseen += 1
-            bi_seen_list.append(bi)
-    prop_bi_unseen = num_bi_unseen / num_bi # proportion of unseen bigram words
+	# bigram process
+	num_bi = len(data) - 1
+	num_bi_unseen = 0
+	for i in range(num_bi):
+		bi = list(data[i:i+2])
+		if not bi in bi_seen_list:
+			num_bi_unseen += 1
+			if doUpdate:
+				bi_seen_list.append(bi)
+	prop_bi_unseen = num_bi_unseen / num_bi # proportion of unseen bigram words
 
-    # trigram process
-    num_tri = len(data) - 2
-    num_tri_unseen = 0
-    for i in range(num_tri):
-        tri = list(data[i:i+3])
-        if not tri in tri_seen_list:
-            num_tri_unseen += 1
-            tri_seen_list.append(tri)
-    prop_tri_unseen = num_tri_unseen / num_tri # proportion of unseen trigram words
+	# trigram process
+	num_tri = len(data) - 2
+	num_tri_unseen = 0
+	for i in range(num_tri):
+		tri = list(data[i:i+3])
+		if not tri in tri_seen_list:
+			num_tri_unseen += 1
+			if doUpdate:
+				tri_seen_list.append(tri)
+	prop_tri_unseen = num_tri_unseen / num_tri # proportion of unseen trigram words
 
     # create tensor variable
-    input_feature = Variable(torch.Tensor(np.array([prop_uni_unseen, prop_bi_unseen, prop_tri_unseen])))
-    input_feature = input_feature.view(-1, 3)
+	input_feature = Variable(torch.Tensor(np.array([prop_uni_unseen, prop_bi_unseen, prop_tri_unseen])))
+	input_feature = input_feature.view(-1, 3)
 
-    return input_feature, uni_seen_list, bi_seen_list, tri_seen_list
+	return input_feature
 
 # Set up DQN
 input_dim = 3 # Three features(unigram, bigram, trigram)
@@ -73,16 +62,16 @@ hidden_dropout_prob = 0.2
 class DQN(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_size=400, hidden_dropout_prob=0.2):
         super(DQN, self).__init__()
-        # self.fc1 = nn.Linear(input_dim, output_dim) # input layer -> output layer
-        self.fc1 = nn.Linear(input_dim, hidden_size) # input layer -> hidden layer
+        self.fc1 = nn.Linear(input_dim, output_dim) # input layer -> output layer
+        # self.fc1 = nn.Linear(input_dim, hidden_size) # input layer -> hidden layer
         # self.fc1_drop = nn.Dropout(p=hidden_dropout_prob) # set the dropout
-        self.fc2 = nn.Linear(hidden_size, output_dim) # hidden layer -> output layer
+        # self.fc2 = nn.Linear(hidden_size, output_dim) # hidden layer -> output layer
         
     def forward(self, x):
         # x = F.sigmoid(self.fc1(x))
         x = F.sigmoid(self.fc1(x))
         # x = self.fc1_drop(x)
-        x = self.fc2(x)
+        # x = self.fc2(x)
         return x
         
 model = DQN(
@@ -115,7 +104,7 @@ def sample(sample_num, dqn_model):
             data_list.append(data)
             
             # Construct the state (how different our input is from the dataset_train, represented as scalar values) w/o updating seen lists
-            state, _,_,_ = create_feature(data, uni_seen_list, bi_seen_list, tri_seen_list)
+            state = create_feature(data, uni_seen_list, bi_seen_list, tri_seen_list, False)
             
             '''
             if j != N_options-1:
@@ -132,7 +121,7 @@ def sample(sample_num, dqn_model):
         choice = np.argmax(state_value_list) # Choose data with highest state value to train 
         data_sampled_DQN.append(data_list[choice]) # Add selected data into dataset
         # Update seen lists
-        state, uni_seen_list, bi_seen_list, tri_seen_list = create_feature(data_list[choice], uni_seen_list, bi_seen_list, tri_seen_list)
+        state = create_feature(data_list[choice], uni_seen_list, bi_seen_list, tri_seen_list, True)
 
         choice_random = random.randint(0, N_options-1) # Choose data randomly
         data_sampled_random.append(data_list[choice_random]) # Add selected data into dataset
